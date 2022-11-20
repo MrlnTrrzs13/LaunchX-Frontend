0794b5def1ada7616dca64c7b62edfce94372c09
MrlnTrrzs13 <brand.trrzs13@gmail.com>
2022-11-20 00:45:46 -0600

    "Subiendo practica pokedex"

---
+++ JS/Pokedex.js
@@ -0,0 +1,45 @@
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("../HTML/Imagenes/Pokedex/triste.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            let poketype = data.types;
+            let pokeabi = data.abilities;
+            let pokeatak = data.moves;
+            pokeImage(pokeImg);
+            pokeType(poketype);
+            pokeAbi(pokeabi);
+            pokeAtak(pokeatak);
+            console.log(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+const pokeType = (typ) => {
+    const poketipo = document.getElementById("pokedataT");
+    const typename = typ.map((item) => item.type.name);
+    poketipo.innerHTML = typename;
+}
+
+const pokeAbi = (abilities) => {
+    const pokeabilities = document.getElementById("pokedata");
+    const abilitiesname = abilities.map((item) => item.ability.name);
+    pokeabilities.innerHTML = abilitiesname;
+}
