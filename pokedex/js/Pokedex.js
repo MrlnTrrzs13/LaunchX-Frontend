69dcb9ba10bff2dcc385b7a116c5a39e64ec7996
MTorres <brand.trrzs13@gmail.com>
2023-01-31 12:36:29 -0600

    Mejorando el diseño

--- pokedex/js/Pokedex.js
+++ pokedex/js/Pokedex.js
@@ -17,9 +17,12 @@ const fetchPokemon = () => {
             let pokeImg = data.sprites.front_default;
             let poketype = data.types;
             let pokeabi = data.abilities;
+            let pokename = data.name;
+            let pokeids = data.id;
             pokeImage(pokeImg);
-            pokeType(poketype);
-            pokeAbi(pokeabi);
+            pokeAbi(pokeabi, poketype);
+            pokeNombre(pokename);
+            pokeId(pokeids);
             console.log(pokeImg);
         }
     });
@@ -28,16 +31,29 @@ const fetchPokemon = () => {
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
+    pokePhoto.style.width = '220px';
+    pokePhoto.style.height = '220px';
 }
 
-const pokeType = (typ) => {
-    const poketipo = document.getElementById("pokedataT");
-    const typename = typ.map((item) => item.type.name);
-    poketipo.innerHTML = typename;
+const pokeId = (identificador) => {
+    const pkid = document.getElementById("pokeid");
+    pkid.innerHTML = `ID: ${identificador}`;
+    
 }
 
-const pokeAbi = (abilities) => {
-    const pokeabilities = document.getElementById("pokedata");
+const pokeAbi = (abilities,typ) => {
+    const resultado =document.getElementById("pokedata");
+    //const pokeabilities = document.getElementById("pokedata");
+    //const poketipo = document.getElementById("pokedata");
     const abilitiesname = abilities.map((item) => item.ability.name);
-    pokeabilities.innerHTML = abilitiesname;
+    const typename = typ.map((item) => item.type.name);
+    resultado.innerHTML = `Abilidad: ${abilitiesname}
+                           Tipo: ${typename}`;
+    //resultado.innerHTML = `Tipo: ${typename}`;
+}
+
+const pokeNombre = (nombre) => {
+    const pknombre = document.getElementById("pokeNomb");
+    pknombre.innerHTML = `Nombre: ${nombre}`;
+    pknombre.style.fontSize = '14px';
 }
